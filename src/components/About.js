7fea5c2068a2643e68ea5280775246a28cfa22ad
FranciscoGB03 <fgonzalezb@toluca.tecnm.mx>
2021-01-02 19:31:40 -0600

    Se creo el componente de About y su ruta

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About =()=>{
+    return(
+        <div>
+            Este es el componente de acerca de            
+        </div>
+    )
+}
+export default About;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
+import About from './components/About';
 import Home from './components/Home';
 import NoFound from './components/NotFound';
 
 const ROUTES=[
     { exact:true, path:'/', component:Home },
+    { exact:true, path:'/About', component:About},
     { component:NoFound}
 ];
 
