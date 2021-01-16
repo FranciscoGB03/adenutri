837efd277ca64eca7f7132096cbe70af834951e1
frangb <fgonzalezb@toluca.tecnm.mx>
2021-01-15 21:13:55 -0600

    botones de lenguaje

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {useTranslation} from 'react-i18next';
 
 const About =()=>{
-    const [t]=useTranslation('cita');
+    const [t,i18n]=useTranslation('cita');
     
     return(
         <div className="recta">
             {t('pliegue')}
             {t('peso')}
-          
+        <div>
+            <button onClick={()=>i18n.changeLanguage("es")}>Es</button>
+            <button onClick={()=>i18n.changeLanguage("en")}>En</button>
+        </div>
         </div>
     )
 }
